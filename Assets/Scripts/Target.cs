c1ddd1be1a47b0177e653446b85ea53d87b2dd74
Beshoy-Bebawe <118367517+Beshoy-Bebawe@users.noreply.github.com>
2024-02-14 12:24:37 -0500

    I finished 5.2

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -11,6 +11,9 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange  = 4;
     private float ySpawnPos = -6; 
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +21,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
-
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
     // Update is called once per frame
     void Update()
@@ -41,9 +44,11 @@ public class Target : MonoBehaviour
         return new Vector3(Random.Range(-xRange,xRange),ySpawnPos);
     }
 
-    private void OmMouseDown()
+    private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position,explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
     private void OnTriggerEnter(Collider other)
     {
