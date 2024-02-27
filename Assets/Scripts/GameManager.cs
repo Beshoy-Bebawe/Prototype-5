6a37c71acbbcdaf8ba5d6d8abc559560a75e5f82
Beshoy-Bebawe <118367517+Beshoy-Bebawe@users.noreply.github.com>
2024-02-27 12:41:15 -0500

    I am almost done with 5.3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 using TMPro;
 public class GameManager : MonoBehaviour
 {
-    public List <GameObject>targets;
+    public List<GameObject> targets;
     private float spawnRate = 1.0f;
     private int score;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
+       
     }
 
     // Update is called once per frame
@@ -23,15 +27,12 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator SpawnTarget()
     {
-    while(true)
-        {
-         yield return new WaitForSeconds(spawnRate);
-         int index = Random.Range(0 ,targets.Count);
-         Instantiate(targets[index]);
-         
-
-
-        }
+        while(isGameActive)
+            {
+                yield return new WaitForSeconds(spawnRate);
+                int index = Random.Range(0 ,targets.Count);
+                Instantiate(targets[index]);
+            }
 
 
     }
@@ -42,7 +43,12 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + score;
 
     }
-
+ 
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
 
 }       
 
