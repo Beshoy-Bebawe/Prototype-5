99ac03188599dbf6b039b83d94507fc5bcad1e55
Beshoy-Bebawe <118367517+Beshoy-Bebawe@users.noreply.github.com>
2024-02-29 12:47:45 -0500

    I Finished 5.3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
@@ -10,6 +12,7 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public bool isGameActive;
+    public Button restartButton;
     // Start is called before the first frame update
     void Start()
     {
@@ -48,6 +51,14 @@ public class GameManager : MonoBehaviour
     {
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+        
+    }
+    public void RestartGame()
+    {   
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+
     }
 
 }       
